0f3748beffff66cb1a54a460d7888cfe81bcc991
Aaronvern <aaronvernekar@gmail.com>
2025-02-23 11:18:54 +0530

    setup

---
+++ backend/test.py
@@ -0,0 +1,13 @@
+import fastapi
+import uvicorn
+import cv2
+import mediapipe
+import numpy
+import requests
+import aptos_sdk
+import jose
+import multipart
+import wolframclient
+import appwrite
+
+print("All packages imported successfully!")
